62707c106ce085a72a4ee9c9eccf0559e0879b92
Mamedov-V-E <black_snake666@mail.ru>
2019-10-22 16:22:22 +0300

    Scripted auto-commit on change (2019-10-22 16:22:22) by gitwatch.sh

--- src/main/java/ParallelProgrammingLab3/SparkApp.java
+++ src/main/java/ParallelProgrammingLab3/SparkApp.java
@@ -8,6 +8,6 @@ public class SparkApp {
 
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        
+        JavaRDD
     }
 }
