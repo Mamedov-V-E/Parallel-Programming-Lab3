1f55bc005f674dc8ac7c9999f2cd7e3fac22986d
Mamedov-V-E <black_snake666@mail.ru>
2019-10-22 18:56:09 +0300

    Scripted auto-commit on change (2019-10-22 18:56:09) by gitwatch.sh

--- src/main/java/ParallelProgrammingLab3/SparkApp.java
+++ src/main/java/ParallelProgrammingLab3/SparkApp.java
@@ -4,31 +4,41 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.broadcast.Broadcast;
 import scala.Int;
 import scala.Tuple2;
 import scala.Tuple3;
+
+import java.util.Map;
 
 public class SparkApp {
     public static void main(String[] args) {
 
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaRDD<String> flights = sc.textFile("664600583_T_ONTIME_sample.csv");
-        JavaPairRDD<Tuple2<Integer, Integer>, Long> flightsData = flights
+        JavaRDD<String> flightsCSV = sc.textFile("664600583_T_ONTIME_sample.csv");
+        JavaPairRDD<Tuple2<Integer, Integer>, Long> flightsData = flightsCSV
                 .mapToPair(SparkApp::GetNewFlightKeyValuePair);
 
-        JavaPairRDD<Integer, String> 
-        stringAirportDataMap = sc.textFile("L_AIRPORT_ID.csv").collectAsMap;
-
-        final Broadcast<Map<String, AirportData>> airportsBroadcasted = sc.broadcast(stringAirportDataMap);
+        JavaRDD<String> airportsCSV = sc.textFile("L_AIRPORT_ID.csv");
+        JavaPairRDD<Integer, String> airportsData = airportsCSV.mapToPair(SparkApp::GetNewAirportsKeyValuePair);
+        Map<Integer, String> stringAirportDataMap = airportsData.collectAsMap();
+        final Broadcast<Map<Integer, String>> airportsBroadcasted = sc.broadcast(stringAirportDataMap);
+        
     }
 
-    private static Tuple2<Tuple2<Integer, Integer>, Long> GetNewFlightKeyValuePair(String line) {
+    private static Tuple2<Tuple2<Integer, Integer>, Long> GetNewFlightKeyValuePair (String line) {
         String[] parameters = ParseUtils.ParseFlightsLogLine(line);
         String originalAirportID = parameters[ParseUtils.FLIGHTS_LOG_ORIGIN_AIRPORT_ID_PARAM_NUMBER];
         String destinationAirportID = parameters[ParseUtils.FLIGHTS_LOG_DEST_AIRPORT_ID_PARAM_NUMBER];
         String delayString = parameters[ParseUtils.FLIGHTS_LOG_DELAY_PARAM_NUMBER];
         Long delay = (delayString.isEmpty()) ? 0 : Long.parseLong(delayString);
         return new Tuple2<>(new Tuple2<>(Integer.parseInt(originalAirportID), Integer.parseInt(destinationAirportID)), delay);
+    }
+
+    private static Tuple2<Integer, String> GetNewAirportsKeyValuePair (String line) {
+        String[] parameters = ParseUtils.ParseAirportsListLine(line);
+        return new Tuple2<>(Integer.parseInt(parameters[ParseUtils.AIRPORTS_LIST_AIRPORT_ID_PARAM_NUMBER]),
+                parameters[ParseUtils.AIRPORTS_LIST_AIRPORT_DESCRIPTION_PARAM_NUMBER]);
     }
 }
