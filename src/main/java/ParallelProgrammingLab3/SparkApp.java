94a682a4a4a03dbe6c9ec3d618a83a415bcab516
Mamedov-V-E <black_snake666@mail.ru>
2019-10-22 18:48:41 +0300

    Scripted auto-commit on change (2019-10-22 18:48:41) by gitwatch.sh

--- src/main/java/ParallelProgrammingLab3/SparkApp.java
+++ src/main/java/ParallelProgrammingLab3/SparkApp.java
@@ -4,6 +4,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Int;
 import scala.Tuple2;
 import scala.Tuple3;
 
@@ -13,20 +14,21 @@ public class SparkApp {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> flights = sc.textFile("664600583_T_ONTIME_sample.csv");
-        JavaPairRDD<Tuple2<String, String>, Long> flightsData = flights
+        JavaPairRDD<Tuple2<Integer, Integer>, Long> flightsData = flights
                 .mapToPair(SparkApp::GetNewFlightKeyValuePair);
 
+        JavaPairRDD<Integer, String> 
         stringAirportDataMap = sc.textFile("L_AIRPORT_ID.csv").collectAsMap;
-        
+
         final Broadcast<Map<String, AirportData>> airportsBroadcasted = sc.broadcast(stringAirportDataMap);
     }
 
-    private static Tuple2<Tuple2<String, String>, Long> GetNewFlightKeyValuePair(String line) {
+    private static Tuple2<Tuple2<Integer, Integer>, Long> GetNewFlightKeyValuePair(String line) {
         String[] parameters = ParseUtils.ParseFlightsLogLine(line);
         String originalAirportID = parameters[ParseUtils.FLIGHTS_LOG_ORIGIN_AIRPORT_ID_PARAM_NUMBER];
         String destinationAirportID = parameters[ParseUtils.FLIGHTS_LOG_DEST_AIRPORT_ID_PARAM_NUMBER];
         String delayString = parameters[ParseUtils.FLIGHTS_LOG_DELAY_PARAM_NUMBER];
         Long delay = (delayString.isEmpty()) ? 0 : Long.parseLong(delayString);
-        return new Tuple2<>(new Tuple2<>(originalAirportID, destinationAirportID), delay);
+        return new Tuple2<>(new Tuple2<>(Integer.parseInt(originalAirportID), Integer.parseInt(destinationAirportID)), delay);
     }
 }
