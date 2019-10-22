9becfa78ebd4882da14a7d76cb33ccc15fcbc017
Mamedov-V-E <black_snake666@mail.ru>
2019-10-22 16:43:46 +0300

    Scripted auto-commit on change (2019-10-22 16:43:46) by gitwatch.sh

--- src/main/java/ParallelProgrammingLab3/SparkApp.java
+++ src/main/java/ParallelProgrammingLab3/SparkApp.java
@@ -5,6 +5,7 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
+import scala.Tuple3;
 
 public class SparkApp {
     public static void main(String[] args) {
@@ -12,6 +13,6 @@ public class SparkApp {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> flights = sc.textFile("664600583_T_ONTIME_sample.csv");
-        JavaPairRDD<Tuple2<String, String>, >
+        JavaPairRDD<Tuple2<String, String>, Tuple3 <>>
     }
 }
